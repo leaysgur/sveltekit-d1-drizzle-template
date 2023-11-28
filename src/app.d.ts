51a840b6ec1a2a4c04abda57329a4bfb5f59c783
Yuji Sugiura <y.sugiura.0316@gmail.com>
2023-11-28 11:54:25 +0900

    Fix examples

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,6 +6,7 @@ declare global {
     interface Platform {
       env: {
         DB: D1Database;
+        KVS: KVNamespace;
       };
     }
   }
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,24 @@
-import { createBridge } from "cfw-bindings-wrangler-bridge";
+import { getBindings } from "cfw-bindings-wrangler-bridge";
+import { unstable_dev } from "wrangler";
 import { dev } from "$app/environment";
 import type { Handle } from "@sveltejs/kit";
+import type { UnstableDevWorker } from "wrangler";
 
+let worker: UnstableDevWorker;
+let env: App.Platform["env"];
 export const handle: Handle = async ({ event, resolve }) => {
   if (dev) {
-    const bridge = createBridge();
-    event.platform = {
-      env: {
-        DB: bridge.D1Database("DB"),
-      },
-    };
+    if (!env) {
+      worker = await unstable_dev(
+        "./node_modules/cfw-bindings-wrangler-bridge/worker/index.js",
+        { experimental: { disableExperimentalWarning: true } },
+      );
+      env = await getBindings({
+        bridgeWorkerOrigin: `http://${worker.address}:${worker.port}`,
+      });
+    }
+
+    event.platform = { env };
   }
 
   return resolve(event);
